b32c820df341b5c651f933fd4d2bf1d524138c17
Larry Tang <realrussianmanconfirmed@gmail.com>
2019-06-28 17:35:07 +0100

    Added a blank unit test app for later use.

---
+++ Algo/AlgoUnitTests/Test.cs
@@ -0,0 +1,13 @@
+﻿using NUnit.Framework;
+using System;
+namespace AlgoUnitTests
+{
+    [TestFixture()]
+    public class Test
+    {
+        [Test()]
+        public void TestCase()
+        {
+        }
+    }
+}
