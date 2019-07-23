4a305c34b99545e026b3dce33b11c5ae48b94c61
Larry Tang <realrussianmanconfirmed@gmail.com>
2019-07-23 23:05:58 +0100

    Add more detail to web responses from web.get().

--- Algo/Standard Library/Libraries/AlgoStd_Web.cs
+++ Algo/Standard Library/Libraries/AlgoStd_Web.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Net;
+using System.Numerics;
 using Antlr4.Runtime;
 
 namespace Algo.StandardLibrary
@@ -34,6 +35,8 @@ namespace Algo.StandardLibrary
 
             //Attempt to perform a GET request to this URL.
             string res = "";
+            string stat = "";
+            int code = -1;
             try
             {
                 HttpWebRequest request = (HttpWebRequest)WebRequest.Create((string)args[0].Value);
@@ -44,6 +47,8 @@ namespace Algo.StandardLibrary
                 using (StreamReader reader = new StreamReader(stream))
                 {
                     res = reader.ReadToEnd();
+                    stat = response.StatusDescription;
+                    code = (int)response.StatusCode;
                 }
 
             }
@@ -53,12 +58,34 @@ namespace Algo.StandardLibrary
                 return null;
             }
 
-            //Return the response.
-            return new AlgoValue()
+            //Create the response object.
+            AlgoScopeCollection obj = new AlgoScopeCollection();
+            obj.AddVariable("content", new AlgoValue()
             {
                 Type = AlgoValueType.String,
                 Value = res
+            });
+            obj.AddVariable("status", new AlgoValue()
+            {
+                Type = AlgoValueType.Integer,
+                Value = new BigInteger(code)
+            });
+            obj.AddVariable("status_desc", new AlgoValue()
+            {
+                Type = AlgoValueType.String,
+                Value = stat
+            });
+
+            //Return the response object.
+            return new AlgoValue()
+            {
+                Type = AlgoValueType.Object,
+                Value = new AlgoObject()
+                {
+                    ObjectScopes = obj
+                }
             };
+
         }
     }
 }
