e9f0b4e3974aa05c0ad7ad2540457395b0dc5883
C272 <realrussianmanconfirmed@gmail.com>
2019-08-20 17:43:00 +0100

    Switch random seed to be based on GUID hash codes.

--- Algo/Standard Library/Libraries/AlgoStd_Maths.cs
+++ Algo/Standard Library/Libraries/AlgoStd_Maths.cs
@@ -19,6 +19,14 @@ namespace Algo.StandardLibrary
                 Name = "random_int",
                 Function = GenerateRandomInteger,
                 ParameterCount = 1
+            },
+
+            //maths.modPow()
+            new AlgoPluginFunction()
+            {
+                Name = "modPow",
+                Function = ModPow,
+                ParameterCount = 3
             }
         };
 
@@ -40,11 +48,32 @@ namespace Algo.StandardLibrary
             }
 
             //It is, generate an integer and return.
-            Random rand = new Random(Environment.TickCount - int.Parse((args[0].Value).ToString()));
+            Random rand = new Random(Guid.NewGuid().GetHashCode() - int.Parse((args[0].Value).ToString()));
             return new AlgoValue()
             {
                 Type = AlgoValueType.Integer,
                 Value = new BigInteger(rand.Next())
+            };
+        }
+
+        //Performs ModPow on two large numbers, useful when powers are too large to process.
+        //args[0] = base
+        //args[1] = exponent
+        //args[2] = modulus
+        public static AlgoValue ModPow(ParserRuleContext context, params AlgoValue[] args)
+        {
+            //Check arguments.
+            if (args[0].Type != args[1].Type || args[1].Type != args[2].Type || args[0].Type != AlgoValueType.Integer)
+            {
+                Error.Fatal(context, "Arguments for ModPow must all be integers.");
+                return null;
+            }
+
+            //Computing.
+            return new AlgoValue()
+            {
+                Type = AlgoValueType.Integer,
+                Value = BigInteger.ModPow((BigInteger)args[0].Value, (BigInteger)args[1].Value, (BigInteger)args[2].Value)
             };
         }
     }
