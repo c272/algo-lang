3c3b41262ded2f6529ea79e0c7372e77fec4e403
C272 <realrussianmanconfirmed@gmail.com>
2019-08-23 19:07:15 +0100

    Algo scripts are now compileable!

---
+++ Algo/ALEC/ALECTemplates.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algo
+{
+    //A collection of all templates required for compiling with ALEC.
+    public static class ALECTemplates
+    {
+    }
+}
